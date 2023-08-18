90b41f1f31bfafa401fb0aaef87dae88334d1e1f
augustobarradas <112783194+augustobarradas@users.noreply.github.com>
2023-08-18 15:23:46 -0300

    TIrei a alteracao do quadrado azul

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ const logo = document.querySelector('.logo__imagem');
 function onClickLogo() {
     document.location.pathname = '/';
 }
-logo.addEventListener('click', onClickLogo)
+logo.addEventListener('click', onClickLogo);
 
 // const pageName = document.location.pathname.slice(1)
 // const currentPageLink = document.querySelector(`a[href="${pageName}"]`)
 // currentPageLink.classList.add('active')
 
-const elemento = document.querySelector(".banner__superior");
-elemento.classList.add("bannerSuperiorJs");
+// Testes com adicao de classes e fazendo um quadrado
 
-const quadrado = document.querySelector(".quadrado__propaganda");
-quadrado.style.width = "100px";
-quadrado.style.height = "100px";
-quadrado.style.backgroundColor = "blue";
+// const elemento = document.querySelector(".banner__superior");
+// elemento.classList.add("bannerSuperiorJs");
+
+// const quadrado = document.querySelector(".quadrado__propaganda");
+// quadrado.style.width = "100px";
+// quadrado.style.height = "100px";
+// quadrado.style.backgroundColor = "blue";
+
+
+const bannerTeste = document.querySelector('.quadrado__propaganda--imagem--logo');
+
+bannerTeste.addEventListener('click', onClickLogo);
 
 
