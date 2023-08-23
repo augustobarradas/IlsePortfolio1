efa56cc46d8fbcaebdd554d188fef099b9e273bb
augustobarradas <112783194+augustobarradas@users.noreply.github.com>
2023-08-23 08:12:58 -0300

    Limpeza de código

--- main.js
+++ main.js
@@ -5,28 +5,6 @@ function onClickLogo() {
 }
 logo.addEventListener('click', onClickLogo);
 
-// const pageName = document.location.pathname.slice(1)
-// const currentPageLink = document.querySelector(`a[href="${pageName}"]`)
-// currentPageLink.classList.add('active')
-
-// Testes com adicao de classes e fazendo um quadrado
-
-// const elemento = document.querySelector(".banner__superior");
-// elemento.classList.add("bannerSuperiorJs");
-
-// const quadrado = document.querySelector(".quadrado__propaganda");
-// quadrado.style.width = "100px";
-// quadrado.style.height = "100px";
-// quadrado.style.backgroundColor = "blue";
-
-// Abaixo tentei replicar o que foi feito lá em cima para mexer o banner.
-// Testei do jeito que esta abaixo e coloquei as barras, funcionou do mesmo jeito pq fiz as alteracoes no Header.css. Entao sei se esse código serve pra alguma coisa kkkk
-
-// const bannerTeste = document.querySelector('.quadrado__propaganda');
-
-// bannerTeste.addEventListener('click', onClickLogo);
-
-
 const banner = document.querySelector(".quadrado__propaganda");
 const images = banner.getElementsByTagName("img");
 let currentImageIndex = 0;
