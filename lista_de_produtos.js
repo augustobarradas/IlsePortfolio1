edf09878eb9e3e9bd7a6a894ea1ff09cdb9b72cb
augustobarradas <112783194+augustobarradas@users.noreply.github.com>
2023-09-08 20:23:47 -0300

    paginas de produtos gerados dinamicamente com js

---
+++ lista_de_produtos.js
@@ -0,0 +1,32 @@
+const products = 
+{
+    espelhos:[
+        {
+            name: 'espelho1', 
+            imgUrl: './img_dos_produtos/Espelho/espelho1.jpg',
+        },
+        {
+            name: 'espelho2', 
+            imgUrl: './img_dos_produtos/Espelho/espelho2.jpg',
+        },
+        {
+            name: 'espelho3', 
+            imgUrl: '/img_dos_produtos/Espelho/espelho3.jpg',
+        },
+
+    ],
+    bolsas:[
+        {
+            name: 'bolsa1', 
+            imgUrl: './img_dos_produtos/bolsas/bolsas1.jpg',
+        },
+        {
+            name: 'bolsa2', 
+            imgUrl: './img_dos_produtos/bolsas/bolsas2.jpg',
+        },
+        {
+            name: 'bolsa3', 
+            imgUrl: './img_dos_produtos/bolsas/bolsas3.jpg',
+        },
+    ],
+}
