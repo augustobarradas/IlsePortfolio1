facfff0154e553999c42e01dbbc3cb2e2782862f
augustobarradas <112783194+augustobarradas@users.noreply.github.com>
2023-10-24 07:48:44 -0300

    Ajustando imagens que nao subiam

--- lista_de_produtos.js
+++ lista_de_produtos.js
@@ -3,15 +3,15 @@ const products =
     espelhos:[
         {
             name: 'espelho1', 
-            imgUrl: './img_dos_produtos/Espelho/espelho1.jpg',
+            imgUrl: './img_dos_produtos/espelho/espelho1.jpg',
         },
         {
             name: 'espelho2', 
-            imgUrl: './img_dos_produtos/Espelho/espelho2.jpg',
+            imgUrl: './img_dos_produtos/espelho/espelho2.jpg',
         },
         {
             name: 'espelho3', 
-            imgUrl: '/img_dos_produtos/Espelho/espelho3.jpg',
+            imgUrl: './img_dos_produtos/espelho/espelho3.jpg',
         },
 
     ],
